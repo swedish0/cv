aa778512824393be1df124a75833c88c9b8e6713
SG <121431267+schvedgabor@users.noreply.github.com>
2023-01-04 00:59:28 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -6,6 +6,56 @@ var arrLang = {
     "main4": "Elérhetőség",
     "break1": "",
     "otp1": "OTP Bank – Rendszerszervező – Bankkártya elfogadási terület",
+    "otp2": "Új bankkártyás rendszer bevezetése - Card Suite",
+    "otp3": "Modulok: Billwriter, Reporting Assistant, Partion organizer, IO Manager, Data mart synchronization, Data transfer to DWH",
+    "otp4": "Prometheus & Grafana - metrics",
+    "otp5": "PGP4 - GPG - Encryption",
+    "otp6": "Python fejlesztése - replacer, quering from database",
+    "otp7": "others",
+    "break2": "",
+    "mkb1": "MKB Bank – Rendszerszervező – Vállalati Elektronikus csatorna és CRM rendszerek",
+    "mkb2": "CRM – Microsoft Dynamics AX – ügyintézői Frontend",
+    "mkb3": "Linistry – felhő alapú sorszámhúzó, ügyfélhívó, időpontfoglaló rendszer",
+    "mkb4": "Voice Analytics - hangelemző szoftver",
+    "break3": "",
+    "erste1": "Erste Bank – Rendszerszervező – Rendszertámogatás osztály",
+    "erste2": "ProcDigi - Biometrikus aláírás bevezetésének támogatása",
+    "erste3": "PAD - Kivonatok gyártása",
+    "erste4": "MNBINF16 megfelelés - Intellinx - fraud monitoring systems",
+    "break4": "",
+    "kh1": "K&H Bank – Rendszerszervező – Front-end és integrált alkalmazások csoport",
+    "kh2": "PSD2 – Push Notification Sender, Sandbox – alkalmazástervezése",
+    "kh3": "GDPR – dokumentumok anonimizálása és törlése",
+    "kh4": "MIFID – dokumentumok kivezetése elektronikus csatornákra",
+    "kh5": "ePosta – elektronikus dokumentumkezelő rendszer szervezése",
+    "kh6": "Kivonatbetöltő alkalmazás továbbfejlesztése",
+    "break4": "",
+    "rafi1": "Raiffeisen Bank – Rendszerszervező – e-Channel csoport",
+    "rafi2": "Tesztautomatizálás – Selenium",
+    "rafi3": "Scolvo FWR - Kapcsolattartás a beszállítókkal",
+    "rafi4": "Behajtási folyamat tervezés",
+    "rafi5": "Webchat update",
+    "break5": "",
+    "other0": "Technológiai ismeretek",
+    "other1": "▪ Szoftverfejlesztési módszertanok ismerete, Programozási paradigmák ismerete",
+    "other2": "Képzés",
+    "other3": "▪ Docker tanfolyam",
+    "other4": "▪ DevOps képzés",
+    "study1": "Tanulmányok",
+    "study2": "▪ Verebély László: Villanyszerelő szak[407130407]",
+    "study3": "▪ PTE-TTK: Gazdaságinformatikus Bsc",
+    "study4": "▪ ZSKF: Nemzetközi gazdálkodás Ba",
+    "study5": "▪ Perfekt: Banki szakügyintéző",
+    "study6": "Nyelvtudás",
+    "study7": "▪ Angol: középfokú „C” típusú állami nyelvvizsga",
+    "study8": "▪ Német: alapfokú ismeretek",
+    "study9": "Egyéb ismeretek",
+    "study10": "Jogosítvány",
+    "study11": "2009 - 'B' kategóriás jogosítvány",
+    "study12": "2015 - 'C' kategóriás jogosítvány (teherautó)",
+    "address1": "Lakcím:",
+    "address2": "1033, Budapest, Szőlő u 86.",
+
   },
   "en": {
     "main1": "Professional experience",
@@ -14,6 +64,56 @@ var arrLang = {
     "main4": "Contact",
     "break1": "",
     "otp1": "OTP Bank – Sytem Analyst – Bank card acquiring area",
+    "otp2": "Introduction of a new bank card system - Card Suite",
+    "otp3": "Moduls: Billwriter, Reporting Assistant, Partion organizer, IO Manager, Data mart synchronization, Data transfer to DWH",
+    "otp4": "Prometheus & Grafana - metrics",
+    "otp5": "PGP4 - GPG - Encryption",
+    "otp6": "Python developments - replacer, quering from database",
+    "otp7": "others",
+    "break2": "",
+    "mkb1": "MKB Bank – System Analyst – Corporate electronic channel and CRM systems",
+    "mkb2": "CRM – Microsoft Dynamics AX – frontend application",
+    "mkb3": "Linistry – sequence number provider, customer caller, appointment booking system",
+    "mkb4": "Voice Analytics - audio analysis software",
+    "break3": "",
+    "erste1": "Erste Bank – Rendszerszervező – Rendszertámogatás osztály",
+    "erste2": "ProcDigi - Biometrikus aláírás bevezetésének támogatása",
+    "erste3": "PAD - Kivonatok gyártása",
+    "erste4": "MNBINF16 megfelelés - Intellinx - fraud monitoring systems",
+    "break4": "",
+    "kh1": "K&H Bank – Rendszerszervező – Front-end és integrált alkalmazások csoport",
+    "kh2": "PSD2 – Push Notification Sender, Sandbox – alkalmazástervezése",
+    "kh3": "GDPR – dokumentumok anonimizálása és törlése",
+    "kh4": "MIFID – dokumentumok kivezetése elektronikus csatornákra",
+    "kh5": "ePosta – elektronikus dokumentumkezelő rendszer szervezése",
+    "kh6": "Kivonatbetöltő alkalmazás továbbfejlesztése",
+    "break4": "",
+    "rafi1": "Raiffeisen Bank – Rendszerszervező – e-Channel csoport",
+    "rafi2": "Tesztautomatizálás – Selenium",
+    "rafi3": "Scolvo FWR - Kapcsolattartás a beszállítókkal",
+    "rafi4": "Behajtási folyamat tervezés",
+    "rafi5": "Webchat update",
+    "break5": "",
+    "other0": "IT skills",
+    "other1": "▪ Knowledge of software development methodologies, knowledge of programming paradigms",
+    "other2": "Trainings",
+    "other3": "▪ Docker course",
+    "other4": "▪ DevOps training",
+    "study1": "Studies",
+    "study2": "▪ Verebély László: Electrician [407130407]",
+    "study3": "▪ PTE-TTK: Business informatics Bsc",
+    "study4": "▪ ZSKF: International economics Ba",
+    "study5": "▪ Perfekt: Bank specialist",
+    "study6": "Language skills",
+    "study7": "▪ English: intermediate „C” accredited language exam ",
+    "study8": "▪ German: basic knowledge",
+    "study9": "Other training",
+    "study10": "Driving licence",
+    "study11": "2009 - 'B' category driver's license",
+    "study12": "2015 - 'C' category driver's license (truck)",
+    "address1": "Address:",
+    "address2": "Szőlő street 86. ,Budapest",
+
   }
 };
 
@@ -95,7 +195,7 @@ function mail() {
       document.location.reload();
     }, 2000);
 
-  }
+  };
 
 
 
@@ -112,7 +212,7 @@ const mobil1 = document.getElementById('mobil');
 const hometab = document.getElementById('home-tab');
 const profiletab = document.getElementById('profile-tab');
 const contacttab = document.getElementById('contact-tab');
-const vmi = document.getElementById('reach');
+const reach1 = document.getElementById('reach');
 
 toggle.addEventListener('click', function(){
   toggle1.classList.toggle('bi-brightness-high-fill');
@@ -123,7 +223,7 @@ toggle.addEventListener('click', function(){
       hometab.style.background = 'white';
       profiletab.style.background = 'white';
       contacttab.style.background = 'white';
-      vmi.style.background = 'white';
+      reach1.style.background = 'white';
       mail1.style.color = 'white';
       mobil1.style.color = 'white';
       body.style.transition = '1s';
